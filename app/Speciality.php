9536b1b481966b4a8ce1694d6ef302216053a47f
Alexpechonov <lexa19971997@tut.by>
2016-11-09 14:25:12 +0300

    Created relation to faculty

--- app/Speciality.php
+++ app/Speciality.php
@@ -33,4 +33,13 @@ class Speciality extends Model
     public function getDescription() {
         return $this->description;
     }
+
+    /**
+     * Get the object of faculty
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function faculty(){
+        return $this->belongsTo(Faculty::class);
+    }
 }
