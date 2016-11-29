3d2f0266676a78d04d68abe5ef8bb9eefbfc3e1a
Alexpechonov <lexa19971997@tut.by>
2016-11-29 22:11:03 +0300

    Created migration for teachers

---
+++ database/migrations/2016_11_29_190834_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName', 80);
+            $table->string('middleName', 80);
+            $table->string('lastName', 80);
+            $table->string('department', 80);
+            $table->string('rank', 80);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
